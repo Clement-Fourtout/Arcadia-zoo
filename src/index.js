a22d3668cf84b272369544327b49a6dd512290c6
clement <clems.corsica70@gmail.com>
2023-11-02 13:32:03 +0100

    modification config

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import Occasions from './Routes/Occasions';
 import Services from './Routes/Services';
 import Contact from './Routes/Connexion';
 
-
-
 const router = createBrowserRouter([
   {
     path:"/",
@@ -42,3 +40,23 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
