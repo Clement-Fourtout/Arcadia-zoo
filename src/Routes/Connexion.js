4c4403618b00932d6b95c2055bc3b30ae5f1e697
clement <clems.corsica70@gmail.com>
2024-05-10 21:25:53 +0200

    delete

--- src/Routes/Connexion.js
+++ src/Routes/Connexion.js
@@ -100,6 +100,7 @@ body {
     background-color: #6495ED;
     color: #fff;
     border-radius: 25px;
+    border: 1px solid #6495ED;
     box-shadow: 3px 3px 3px #b1b1b1,
         -3px -3px 3px #fff;
     letter-spacing: 1.3px;
@@ -107,6 +108,23 @@ body {
 
 .wrapper .btn1:hover {
     background-color: #003399;
+}
+
+.wrapper .btn2 {
+    box-shadow: none;
+    width: 100%;
+    height: 40px;
+    background-color: #A52A2A;
+    color: #fff;
+    border-radius: 25px;
+    border: 1px solid #A52A2A;
+    box-shadow: 3px 3px 3px #b1b1b1,
+        -3px -3px 3px #fff;
+    letter-spacing: 1.3px;
+}
+
+.wrapper .btn2:hover {
+    background-color: #800000;
 }
 
 .wrapper a {
@@ -137,6 +155,29 @@ if (style.styleSheet){
 
 // Ajoutez l'élément style au head du document
 head.appendChild(style);
+
+const deleteUser = async (nom, mot_de_passe) => {
+    try {
+        const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/users/delete`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ nom, mot_de_passe })
+        });
+
+        if (response.ok) {
+            const { message } = await response.json();
+            console.log(message); // Affichez un message de succès
+        } else {
+            const { error } = await response.json();
+            console.error('Erreur lors de la suppression de l\'utilisateur :', error);
+        }
+    } catch (error) {
+        console.error('Erreur lors de la suppression de l\'utilisateur :', error);
+    }
+};
+
 
 const register = async (nom, mot_de_passe) => {
     try {
@@ -200,7 +241,12 @@ export default function Connexion() {
         await register(nom, mot_de_passe);
     };
 
+    const handleDeleteUser = async (event) => {
+        event.preventDefault();
 
+        // Appel de la fonction deleteUser avec le nom d'utilisateur
+        await deleteUser(nom);
+    };
     return (
         <>
             <div className="wrapper">
@@ -237,6 +283,7 @@ export default function Connexion() {
                     </div>
                     <button type="submit" className="btn mt-3">Connexion</button>
                     <button type="button" onClick={handleRegister} className="btn1 mt-3">Créer un compte</button>
+                    <button type="button" onClick={handleDeleteUser} className="btn2 mt-3">Supprimer un compte</button>
                 </form>
             </div>
         </>
