e07d43597285c76643709ac7d5bdebf214b0807d
clement <clems.corsica70@gmail.com>
2024-05-14 21:21:08 +0200

    delete

--- src/components/Page/admin.js
+++ src/components/Page/admin.js
@@ -173,7 +173,7 @@ const handleRegister = async (event) => {
   }
 };
 
-const handleDeleteAccount = async (userId) => {
+const handleDeleteAccount = async () => {
   try {
       const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/users/${userId}`, {
           method: 'DELETE',
