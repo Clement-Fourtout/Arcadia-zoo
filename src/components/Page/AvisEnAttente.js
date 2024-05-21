075b612772454bb039592aed513673faac4c68b0
clement <clems.corsica70@gmail.com>
2024-05-21 03:21:41 +0200

    Avis

--- src/components/Page/AvisEnAttente.js
+++ src/components/Page/AvisEnAttente.js
@@ -4,25 +4,32 @@ const AvisEnAttente = () => {
   const [avisAttente, setAvisAttente] = useState([]);
 
   useEffect(() => {
+    // Fonction pour récupérer les avis en attente depuis le serveur
     async function fetchAvisAttente() {
-      const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/avis_attente');
-      if (response.ok) {
-        const data = await response.json();
-        console.log('data:', data); // Vérifiez la structure des données
-        setAvisAttente(data); // Ajustez en fonction de la structure des données
-      } else {
-        console.error('Erreur lors de la récupération des avis en attente :', response.statusText);
+      try {
+        const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/avis_attente');
+        if (response.ok) {
+          const data = await response.json();
+          // Stocker les avis en attente dans le stockage local
+          localStorage.setItem('avisAttente', JSON.stringify(data.result));
+          setAvisAttente(data.result);
+        } else {
+          console.error('Erreur lors de la récupération des avis en attente :', response.statusText);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la récupération des avis en attente :', error);
       }
     }
 
-    fetchAvisAttente().catch(error => {
-      if (error instanceof SyntaxError) {
-        console.error('Erreur de syntaxe lors du traitement des données JSON :', error);
-      } else {
-        console.error('Erreur lors de la récupération des avis en attente :', error);
-      }
-    });
-    
+    // Vérifier d'abord s'il existe des données dans le stockage local
+    const storedAvisAttente = localStorage.getItem('avisAttente');
+    if (storedAvisAttente) {
+      // Si des données sont trouvées, les utiliser
+      setAvisAttente(JSON.parse(storedAvisAttente));
+    } else {
+      // Sinon, récupérer les données du serveur
+      fetchAvisAttente();
+    }
   }, []);
 
   return (
