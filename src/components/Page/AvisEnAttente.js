6aba45cbcedf19955545d77d132666a9092fc34e
clement <clems.corsica70@gmail.com>
2024-05-21 03:03:12 +0200

    Avis

--- src/components/Page/AvisEnAttente.js
+++ src/components/Page/AvisEnAttente.js
@@ -5,22 +5,19 @@ const AvisEnAttente = () => {
 
   useEffect(() => {
     async function fetchAvisAttente() {
-      try {
-        const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/avis_attente');
-        if (response.ok) {
-          const data = await response.json();
-          const avisEnAttente = data.result;
-          console.log('data.result : ', data.result); // Accédez aux données à partir de la propriété "result"
-          setAvisAttente(avisEnAttente);
-        } else {
-          console.error('Erreur lors de la récupération des avis en attente :', response.statusText);
-        }
-      } catch (error) {
-        console.error('Erreur lors de la récupération des avis en attente :', error);
+      const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/avis_attente');
+      if (response.ok) {
+        const data = await response.json();
+        console.log('data:', data); // Vérifiez la structure des données
+        setAvisAttente(data); // Ajustez en fonction de la structure des données
+      } else {
+        console.error('Erreur lors de la récupération des avis en attente :', response.statusText);
       }
     }
 
-    fetchAvisAttente();
+    fetchAvisAttente().catch(error => {
+      console.error('Erreur lors de la récupération des avis en attente :', error);
+    });
   }, []);
 
   return (
