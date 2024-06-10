1f7e4b71c92dc024a202dd91046403a4b6b63eea
clement <clems.corsica70@gmail.com>
2024-06-10 05:35:12 +0200

    css header

--- src/Routes/Accueil.js
+++ src/Routes/Accueil.js
@@ -7,36 +7,39 @@ import Top from "../components/styles/Logo/Accessoire/to_top.png"
 
 export default function Accueil() {
   return (
- <>
-     <a id="top" href="top"> </a>
-      <main class="container-fluid bg-dark p-2 mt-1 mb-3">
-      <h2 class="text-center"><a class="btn btn-outline-success " href="parc" role="button">Nos animaux</a></h2>
-      <div class="row justify-content-center">
-        <div class="col-lg-6 col-md-7">
-          <a href="parc">
-          <img src={Chimpanzée} class="img-fluid border border-success rounded mb-3 " alt="chimpanzée" />    
-          </a>
-        </div>
-        <hr></hr>
-        <h2 class="text-center"><a class="btn btn-outline-light " href="services" role="button">Les services du parc</a></h2>
-        <div class="col-lg-6 col-md-7"> 
-        <a href='services'>    
-          <img src={Services} class="img-fluid border border-light rounded mb-3" alt="services" />
-        </a>    
-        </div>
-        <hr></hr>
-        <h2 class="text-center"><a class="btn btn-outline-info" href="arcadia" role="button">Notre histoire</a></h2>
-        <div class="col-lg-6 col-md-7">
-          <a href='arcadia'>
-          <img src={Histoire}  class="img-fluid border border-info rounded mb-3" alt="histoire"  />
-          </a>
-        </div>
+<>
+  <a id="top" href="top"> </a>
+  <main className="container-fluid bg-dark p-2 mt-1 mb-3">
+    <div className="row justify-content-center">
+      <div className="col-lg-6 col-md-7 mb-3">
+        <h2 className="text-center"><a className="btn btn-outline-success" href="parc" role="button">Nos animaux</a></h2>
+        <a href="parc">
+          <img src={Chimpanzée} className="img-fluid border border-success rounded mb-3" alt="chimpanzée" />
+        </a>
       </div>
-      </main>
-      <a href="#top" >
-      <img src={Top} class="" alt="" style={{position: "fixed", bottom : "50px", right: "50px", opacity: 0.5}}/>
-      </a>
-</> 
+    </div>
+    <div className="row justify-content-center">
+      <div className="col-lg-6 col-md-7 mb-3">
+        <h2 className="text-center"><a className="btn btn-outline-light" href="services" role="button">Les services du parc</a></h2>
+        <a href='services'>
+          <img src={Services} className="img-fluid border border-light rounded mb-3" alt="services" />
+        </a>
+      </div>
+    </div>
+    <div className="row justify-content-center">
+      <div className="col-lg-6 col-md-7 mb-3">
+        <h2 className="text-center"><a className="btn btn-outline-info" href="arcadia" role="button">Notre histoire</a></h2>
+        <a href='arcadia'>
+          <img src={Histoire} className="img-fluid border border-info rounded mb-3" alt="histoire" />
+        </a>
+      </div>
+    </div>
+  </main>
+  <a href="#top">
+    <img src={Top} alt="" style={{ position: "fixed", bottom: "50px", right: "50px", opacity: 0.5 }} />
+  </a>
+</>
+
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,19 @@ import Logo from './styles/Logo/Arcadia Zoo.png'
 
 function Header() {
   return (
-    <>
-    <header class="container-fluid bg-white ">
-      <div class="row justify-content-center">
-        <div class="col-lg-1" style={{maxWidth:"30%"}}>
-          <a href='/'>
-          <img src={Logo} class="img-fluid float-start" alt="logo"/>    
-          </a>
-        </div>
-        <div class="col-lg-7 row align-items-center">
-          <p class=" text-xl-center fs-1 fw-bold text-decoration-underline">Leur santé, notre priorité ! </p>
-        </div>
+<>
+  <header className="container-fluid bg-white">
+    <div className="row justify-content-center align-items-center">
+      <div className="col-lg-1 text-center">
+        <a href='/'>
+          <img src={Logo} className="img-fluid max-width-100" alt="logo" style={{ maxWidth: '150px' }}/>    
+        </a>
       </div>
-    </header>
-    </>
+    </div>
+  </header>
+</>
+
+
   );
 }
 
