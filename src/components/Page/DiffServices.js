dcb90798073f046b6d95a7798f68a9d8df695a0d
clement <clems.corsica70@gmail.com>
2024-06-21 14:16:09 +0200

    Services

--- src/components/Page/DiffServices.js
+++ src/components/Page/DiffServices.js
@@ -9,60 +9,55 @@ const DiffServices = () => {
   const [services, setServices] = useState([]);
 
   useEffect(() => {
-    async function fetchServices() {
-      try {
-        const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/services');
-        if (response.ok) {
-          const data = await response.json();
-          setServices(data);
-        } else {
-          console.error('Erreur lors de la récupération des services :', response.statusText);
-        }
-      } catch (error) {
-        console.error('Erreur lors de la récupération des services :', error);
-      }
-    }
-
     fetchServices();
   }, []);
 
+  const fetchServices = async () => {
+    try {
+      const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/services');
+      if (response.ok) {
+        const data = await response.json();
+        setServices(data);
+      } else {
+        console.error('Erreur lors de la récupération des services :', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des services :', error);
+    }
+  };
 
   return (
-    <div className="center-container">
+    <div className="services-container">
       <h1>Les Services du Parc</h1>
-      {services && services.length > 0 ? (
-        <div className="services-container">
-          {services.map(service => (
-            <div key={service.id} className="service-item">
-              <h2>{service.title}</h2>
-              <img src={getImageUrl(service.image_url)} alt={service.title} className="service-image" />
-              <p>{service.description}</p>
-            </div>
-          ))}
+      {services.map(service => (
+        <div key={service.id} className="service-item">
+          <img src={getServiceImage(service.title)} alt={service.title} />
+          <div className="service-details">
+            <h2>{service.title}</h2>
+            <p>{service.description}</p>
+          </div>
         </div>
-      ) : (
-        <p>Aucun service à afficher</p>
-      )}
+      ))}
     </div>
   );
 };
 
 // Fonction utilitaire pour obtenir l'URL complète de l'image en fonction de son nom importé
-function getImageUrl(imageName) {
-  switch (imageName) {
-    case 'Restaurant1':
+const getServiceImage = (title) => {
+  switch (title) {
+    case 'Le Sauvageon':
       return Restaurant1;
-    case 'Restauration':
+    case 'La Serre Gustative':
       return Restauration;
-    case 'Pacha':
+    case 'Le Pacha':
       return Pacha;
     case 'Guide':
       return Guide;
     case 'Train':
       return Train;
     default:
-      return ''; // Gérer le cas où l'image n'est pas trouvée
+      return '';
   }
-}
+};
 
 export default DiffServices;
