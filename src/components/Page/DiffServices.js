dd33e4e416551477dc7b4c80b3065058accc6de5
clement <clems.corsica70@gmail.com>
2024-07-04 21:06:59 +0200

    heroku

--- src/components/Page/DiffServices.js
+++ src/components/Page/DiffServices.js
@@ -7,57 +7,49 @@ import Guide from '../styles/Logo/Services/Guide.jpg'
 import Train from '../styles/Logo/Services/Train.jpg'
 
 const DiffServices = () => {
-  const [services, setServices] = useState([]);
+  const [services, setServices] = React.useState([]);
 
-  useEffect(() => {
-    fetchServices();
+
+
+  React.useEffect(() => {
+    fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/services')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data fetched from API:', data); // Vérifiez le format des données ici
+        setServices(data);
+      })
+      .catch((error) => console.error('Error fetching services:', error));
   }, []);
 
-  const fetchServices = async () => {
-    try {
-      const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/services');
-      if (response.ok) {
-        const data = await response.json();
-        setServices(data);
-      } else {
-        console.error('Erreur lors de la récupération des services :', response.statusText);
-      }
-    } catch (error) {
-      console.error('Erreur lors de la récupération des services :', error);
-    }
-  };
-
-  
-
   return (
-    <>
-      <div className="container-fluid bg-dark p-2 mt-1 mb-3">
-        <div className="row justify-content-center">
-          <div className="col col-lg-5 text-light">
-            <h1 className="text-xl-center text-decoration-underline font-weight-bold mt-5 mb-5">
-              Les Services du Parc
-            </h1>
-          </div>
+    <div className="container-fluid bg-dark p-2 mt-1 mb-3">
+      <div className="row justify-content-center">
+        <div className="col col-lg-5 text-light">
+          <h1 className="text-xl-center text-decoration-underline font-weight-bold mt-5 mb-5">
+            Les Services du Parc
+          </h1>
         </div>
-        {services.map((service) => (
+      </div>
+      {Array.isArray(services) ? (
+        services.map((service) => (
           <div key={service.id} className="row justify-content-center">
             <div className="col-lg-5 text-light text-center">
               <h2 className="font-weight-bold text-decoration-underline text-info mt-3 mb-4">
                 {service.title}
               </h2>
               <img
-                src={`https://api-zoo-22654ce4a3d5.herokuapp.com/uploads/${service.image_url}`}
+                src={`https://api-zoo-22654ce4a3d5.herokuapp.com/${service.image_url}`}
                 className="d-block w-100"
                 alt={service.title}
-
               />
-              
               <p className="mt-2">{service.description}</p>
             </div>
           </div>
-        ))}
-      </div>
-    </>
+        ))
+      ) : (
+        <p className="text-light">Aucun service trouvé.</p>
+      )}
+    </div>
   );
 };
 
