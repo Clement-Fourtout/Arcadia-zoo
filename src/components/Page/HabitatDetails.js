a620ed128390ae3f4fa6bd7c2e733cc59d65081c
clement <clems.corsica70@gmail.com>
2024-07-11 17:02:59 +0200

    add habitat

--- src/components/Page/HabitatDetails.js
+++ src/components/Page/HabitatDetails.js
@@ -3,23 +3,9 @@ import { useParams } from 'react-router-dom';
 
 const HabitatDetails = () => {
   const { id } = useParams();
-  const [habitat, setHabitat] = useState(null);
   const [animals, setAnimals] = useState([]);
 
   useEffect(() => {
-    const fetchHabitat = async () => {
-      try {
-        const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/habitats/${id}`);
-        if (!response.ok) {
-          throw new Error('Erreur lors de la récupération de l\'habitat');
-        }
-        const habitatData = await response.json();
-        setHabitat(habitatData);
-      } catch (error) {
-        console.error('Erreur lors de la récupération de l\'habitat:', error);
-      }
-    };
-
     const fetchAnimalsByHabitat = async () => {
       try {
         const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/animals?habitat_id=${id}`);
@@ -33,20 +19,12 @@ const HabitatDetails = () => {
       }
     };
 
-    fetchHabitat();
     fetchAnimalsByHabitat();
   }, [id]);
 
-  if (!habitat) {
-    return <div>Chargement...</div>;
-  }
-
   return (
     <div className="container">
-      <h1>{habitat.name}</h1>
-      <img src={habitat.image} alt={habitat.name} />
-      <p>{habitat.description}</p>
-      <h2>Animaux présents :</h2>
+      <h1>Liste des Animaux dans cet Habitat</h1>
       <ul>
         {animals.length > 0 ? (
           animals.map((animal) => (
