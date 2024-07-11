7b3f120c7a5fda6055fc1c9f583b745456eb9fb6
clement <clems.corsica70@gmail.com>
2024-07-11 16:57:44 +0200

    add habitat

--- src/components/Page/HabitatDetails.js
+++ src/components/Page/HabitatDetails.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 const HabitatDetails = () => {
   const { id } = useParams();
   const [habitat, setHabitat] = useState(null);
+  const [animals, setAnimals] = useState([]);
 
   useEffect(() => {
     const fetchHabitat = async () => {
@@ -12,14 +13,28 @@ const HabitatDetails = () => {
         if (!response.ok) {
           throw new Error('Erreur lors de la récupération de l\'habitat');
         }
-        const data = await response.json();
-        setHabitat(data);
+        const habitatData = await response.json();
+        setHabitat(habitatData);
       } catch (error) {
-        console.error('Error fetching habitat:', error);
+        console.error('Erreur lors de la récupération de l\'habitat:', error);
+      }
+    };
+
+    const fetchAnimalsByHabitat = async () => {
+      try {
+        const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/animals?habitat_id=${id}`);
+        if (!response.ok) {
+          throw new Error('Erreur lors de la récupération des animaux');
+        }
+        const animalsData = await response.json();
+        setAnimals(animalsData);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des animaux :', error);
       }
     };
 
     fetchHabitat();
+    fetchAnimalsByHabitat();
   }, [id]);
 
   if (!habitat) {
@@ -31,7 +46,18 @@ const HabitatDetails = () => {
       <h1>{habitat.name}</h1>
       <img src={habitat.image} alt={habitat.name} />
       <p>{habitat.description}</p>
-      <p>Animaux présents : {habitat.animal_list}</p>
+      <h2>Animaux présents :</h2>
+      <ul>
+        {animals.length > 0 ? (
+          animals.map((animal) => (
+            <li key={animal.id}>
+              {animal.name} - {animal.species} - Âge : {animal.age}
+            </li>
+          ))
+        ) : (
+          <p>Aucun animal trouvé pour cet habitat</p>
+        )}
+      </ul>
     </div>
   );
 };
