4d2a1a0c50b6b9cef95fb5a564aaf3225ee103f3
clement <clems.corsica70@gmail.com>
2024-07-11 17:09:55 +0200

    add habitat

--- src/components/Page/HabitatDetails.js
+++ src/components/Page/HabitatDetails.js
@@ -23,15 +23,15 @@ const HabitatDetails = () => {
   }, [id]);
 
   return (
-    <div className="container">
+    <div className="container-fluid bg-dark p-2 mt-1 mb-3 text-center ">
       <h1>Animaux présents dans l'habitat</h1>
       <ul>
         {animals.length > 0 ? (
           animals.map((animal) => (
             <li key={animal.id}>
-              <h2>{animal.name}</h2>
-              <p>Espèce : {animal.species}</p>
-              <p>Âge : {animal.age}</p>
+              <h3 class="text-xl text-align-center text-light text-decoration-underline mt-3">{animal.name}</h3>
+              <p class="text-light text-left mt-3">Espèce : {animal.species}</p>
+              <p class="text-light text-left mt-3">Âge : {animal.age}</p>
               {animal.image && <img src={animal.image} alt={animal.name} style={{ maxWidth: '200px', maxHeight: '200px' }} />}
             </li>
           ))
