774e138509d1256d0206d772ff0ab1e151d74f97
clement <clems.corsica70@gmail.com>
2024-07-11 17:18:53 +0200

    add habitat

--- src/components/Page/HabitatDetails.js
+++ src/components/Page/HabitatDetails.js
@@ -3,42 +3,46 @@ import { useParams } from 'react-router-dom';
 
 const HabitatDetails = () => {
   const { id } = useParams();
-  const [animals, setAnimals] = useState([]);
+  const [habitat, setHabitat] = useState(null);
 
   useEffect(() => {
-    const fetchAnimalsByHabitat = async () => {
+    const fetchHabitat = async () => {
       try {
-        const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/animals?habitat_id=${id}`);
+        const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/habitats/${id}`);
         if (!response.ok) {
-          throw new Error('Erreur lors de la récupération des animaux');
+          throw new Error('Erreur lors de la récupération de l\'habitat');
         }
-        const animalsData = await response.json();
-        setAnimals(animalsData);
+        const data = await response.json();
+        setHabitat(data);
       } catch (error) {
-        console.error('Erreur lors de la récupération des animaux :', error);
+        console.error('Error fetching habitat:', error);
       }
     };
 
-    fetchAnimalsByHabitat();
+    fetchHabitat();
   }, [id]);
 
+  if (!habitat) {
+    return <div>Chargement...</div>;
+  }
+
   return (
-    <div className="container-fluid bg-dark p-2 mt-1 mb-3 text-center ">
-      <h1>Animaux présents dans l'habitat</h1>
-      <ul>
-        {animals.length > 0 ? (
-          animals.map((animal) => (
-            <li key={animal.id}>
-              <h3 class="text-xl text-align-center text-light text-decoration-underline mt-3">{animal.name}</h3>
-              <p class="text-light text-left mt-3">Espèce : {animal.species}</p>
-              <p class="text-light text-left mt-3">Âge : {animal.age}</p>
-              {animal.image && <img src={animal.image} alt={animal.name} style={{ maxWidth: '200px', maxHeight: '200px' }} />}
-            </li>
-          ))
-        ) : (
-          <p>Aucun animal trouvé pour cet habitat</p>
-        )}
-      </ul>
+    <div className="container-fluid bg-dark p-2 mt-1 mb-3 text-center">
+      <h1 className="text-xl-center text-custom-savane text-decoration-underline font-weight-bold" style={{ marginBottom: "50px", marginTop: "25px" }}>
+        {habitat.name}
+      </h1>
+      <div className="row justify-content-center mb-2">
+        {habitat.animals.map((animal) => (
+          <div key={animal.id} className="col-lg-4 mb-4">
+            <img src={animal.image} className="col-lg-11 rounded" alt={animal.name} style={{ marginLeft: 0, padding: 0 }} />
+            <div className="col-lg-12 border">
+              <h3 className="text-xl text-align-center text-light text-decoration-underline mt-3">{animal.name}</h3>
+              <h5 className="text-light text-left mt-3"><em>Espèce :</em> {animal.species}</h5>
+              <h5 className="text-light text-left mt-3"><em>Âge :</em> {animal.age}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
