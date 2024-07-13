419189135eeb2dc522555218dc3d8c0e3f8f94a8
clement <clems.corsica70@gmail.com>
2024-07-13 19:45:42 +0200

    add VetRecords

--- src/components/Page/admin.js
+++ src/components/Page/admin.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import Logo from '../styles/Logo/Arcadia Zoo.png';
 import Nav from '../Nav';
 import AvisEnAttente from './AvisEnAttente';
+import { useParams } from 'react-router-dom';
 
 export default function Admin() {
   const [nom, setNom] = useState('');
@@ -17,8 +18,16 @@ export default function Admin() {
   const [newHabitat, setNewHabitat] = useState({name: '', description: '', image: '', animal_list: '' });
   const [newAnimal, setNewAnimal] = useState({name: '', species: '', age: '', habitat_id: '',image: null });
   const [animal, setAnimal] = useState([]);
-
   const [animals, setAnimals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [newVetRecord, setNewVetRecord] = useState({
+    health_status: '',
+    food: '',
+    food_amount: '',
+    visit_date: '',
+    details: ''
+  });
+  const { id } = useParams();
 
   useEffect(() => {
     async function fetchUserData() {
@@ -559,37 +568,23 @@ const handleDeleteAnimal = async (animalId) => {
 
 // Récupérer la liste des animaux depuis l'API
 useEffect(() => {
-  fetchAnimals();
-}, []);
-
-const fetchAnimals = async () => {
-  try {
-    const response = await fetch('https://api-zoo-22654ce4a3d5.herokuapp.com/animals');
-    if (!response.ok) {
-      throw new Error('Erreur lors du chargement des animaux');
-    }
-    const data = await response.json();
-
-    // Récupérer les détails des animaux avec leurs enregistrements vétérinaires
-    const animalsWithVetRecords = await Promise.all(data.map(async (animal) => {
-      const animalResponse = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/animals/${animal.id}`);
-      if (!animalResponse.ok) {
-        throw new Error(`Erreur lors du chargement des détails de l'animal ${animal.id}`);
+  const fetchAnimal = async () => {
+    try {
+      const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/animals/${id}`);
+      if (!response.ok) {
+        throw new Error('Erreur lors de la récupération de l\'animal');
       }
-      const animalData = await animalResponse.json();
-      return animalData;
-    }));
+      const data = await response.json();
+      setAnimal(data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'animal :', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    setAnimals(animalsWithVetRecords);
-  } catch (error) {
-    console.error('Erreur lors du chargement des animaux :', error);
-    // Gérer les erreurs ici
-  }
-};
-
-if (animals.length === 0) {
-  return <div>Chargement...</div>;
-}
+  fetchAnimal();
+}, [id]);
 
 const handleDeleteVetRecord = async (vetRecordId) => {
   try {
@@ -602,23 +597,65 @@ const handleDeleteVetRecord = async (vetRecordId) => {
     }
 
     // Mettre à jour l'état pour refléter la suppression
-    setAnimal(prevAnimal => {
+    setAnimal((prevAnimal) => {
       if (!prevAnimal || !prevAnimal.vetRecords) {
         return prevAnimal;
       }
       return {
         ...prevAnimal,
-        vetRecords: prevAnimal.vetRecords.filter(record => record.id !== vetRecordId),
+        vetRecords: prevAnimal.vetRecords.filter((record) => record.id !== vetRecordId),
       };
     });
   } catch (error) {
     console.error('Erreur lors de la suppression de l\'enregistrement vétérinaire :', error);
-    // Gérer les erreurs ici
   }
 };
 
-if (!animal) {
+const handleAddVetRecord = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await fetch(`https://api-zoo-22654ce4a3d5.herokuapp.com/vetrecords`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...newVetRecord, animal_id: id }),
+    });
+
+    if (!response.ok) {
+      throw new Error('Erreur lors de l\'ajout de l\'enregistrement vétérinaire');
+    }
+
+    const addedRecord = await response.json();
+
+    // Mettre à jour l'état pour refléter l'ajout
+    setAnimal((prevAnimal) => {
+      if (!prevAnimal) return prevAnimal;
+      return {
+        ...prevAnimal,
+        vetRecords: [...prevAnimal.vetRecords, addedRecord],
+      };
+    });
+
+    // Réinitialiser le formulaire
+    setNewVetRecord({
+      health_status: '',
+      food: '',
+      food_amount: '',
+      visit_date: '',
+      details: ''
+    });
+  } catch (error) {
+    console.error('Erreur lors de l\'ajout de l\'enregistrement vétérinaire :', error);
+  }
+};
+
+if (isLoading) {
   return <div>Chargement...</div>;
+}
+
+if (!animal) {
+  return <div>Erreur de chargement des données de l'animal.</div>;
 }
 
 return (
@@ -897,6 +934,65 @@ return (
           )}
         </div>
       ))}
+    </div>
+    <div>
+    <h2 className="text-light text-decoration-underline mt-4">Ajouter un enregistrement vétérinaire</h2>
+      <form onSubmit={handleAddVetRecord}>
+        <div className="form-group">
+          <label htmlFor="health_status" className="text-light">État de santé</label>
+          <input
+            type="text"
+            id="health_status"
+            className="form-control"
+            value={newVetRecord.health_status}
+            onChange={(e) => setNewVetRecord({ ...newVetRecord, health_status: e.target.value })}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="food" className="text-light">Nourriture proposée</label>
+          <input
+            type="text"
+            id="food"
+            className="form-control"
+            value={newVetRecord.food}
+            onChange={(e) => setNewVetRecord({ ...newVetRecord, food: e.target.value })}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="food_amount" className="text-light">Grammage de la nourriture</label>
+          <input
+            type="text"
+            id="food_amount"
+            className="form-control"
+            value={newVetRecord.food_amount}
+            onChange={(e) => setNewVetRecord({ ...newVetRecord, food_amount: e.target.value })}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="visit_date" className="text-light">Date de visite</label>
+          <input
+            type="date"
+            id="visit_date"
+            className="form-control"
+            value={newVetRecord.visit_date}
+            onChange={(e) => setNewVetRecord({ ...newVetRecord, visit_date: e.target.value })}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="details" className="text-light">Détails</label>
+          <textarea
+            id="details"
+            className="form-control"
+            value={newVetRecord.details}
+            onChange={(e) => setNewVetRecord({ ...newVetRecord, details: e.target.value })}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-2">Ajouter</button>
+      </form>
     </div>
       </div>
     </>
