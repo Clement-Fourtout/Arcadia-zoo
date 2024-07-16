54108c98cf655b8f9d9db7d14b92aa36a86adb91
clement <clems.corsica70@gmail.com>
2024-07-16 18:08:02 +0200

    Mongoose

--- src/components/Page/HabitatDetails.js
+++ src/components/Page/HabitatDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import '../styles/CSS/Animaux/HabitatDetails.css'
+import '../styles/CSS/Animaux/HabitatDetails.css';
+
 const HabitatDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
