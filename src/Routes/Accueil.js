07f63213269480763d533268156ec5dc1a38a23e
clement <clems.corsica70@gmail.com>
2024-07-17 17:47:51 +0200

    Edit Animal

--- src/Routes/Accueil.js
+++ src/Routes/Accueil.js
@@ -3,8 +3,8 @@ import Chimpanzée from '../components/styles/Logo/Animaux/Chimpanzée.jpg'
 import Services from '../components/styles/Logo/Services/Restauration.jpg'
 import Histoire from '../components/styles/Logo//Histoire/Foret de brocéliande.jpg'
 import Top from "../components/styles/Logo/Accessoire/to_top.png"
-import AvisList from '../components/Page/AvisList';
-
+import AvisList from '../components/Page/AvisList'
+import Avis from '../components/Page/Avis'
 export default function Accueil() {
   return (
 <>
@@ -39,6 +39,7 @@ export default function Accueil() {
     <img src={Top} alt="" style={{ position: "fixed", bottom: "50px", right: "50px", opacity: 0.5 }} />
   </a>
   <AvisList></AvisList>
+  <Avis></Avis>
 </>
 
   )
