7662595abc7b8a2abbccb07d75af5e7477d2675f
clement <clems.corsica70@gmail.com>
2024-07-18 17:07:08 +0200

    Mongoose

--- src/Routes/ProtectedRoute.js
+++ src/Routes/ProtectedRoute.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ element: Element, isAuthenticated, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            element={isAuthenticated ? <Element /> : <Navigate to="/login" />}
-        />
+const ProtectedRoute = ({ element, ...rest }) => {
+    // Votre logique d'authentification ici
+    const isAuthenticated = !!localStorage.getItem('token');
+
+    return isAuthenticated ? (
+        <Route {...rest} element={element} />
+    ) : (
+        <Navigate to="/connexion" replace state={{ from: rest.location }} />
     );
 };
 
